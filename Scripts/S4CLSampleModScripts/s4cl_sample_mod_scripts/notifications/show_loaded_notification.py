378200cbe6feb8758db36a61e3f12ce50ccd5275
ColonolNutty <ColonolNutty@hotmail.com>
2020-03-01 12:20:41 -0700

    Updates:
    - Make the scripts easier to read.

--- Scripts/S4CLSampleModScripts/s4cl_sample_mod_scripts/notifications/show_loaded_notification.py
+++ Scripts/S4CLSampleModScripts/s4cl_sample_mod_scripts/notifications/show_loaded_notification.py
@@ -7,12 +7,18 @@ from sims4communitylib.notifications.common_basic_notification import CommonBasi
 class S4CLSampleModShowLoadedMessage:
     """ A class that listens for a zone load event and shows a notification upon loading into a household. """
     @staticmethod
-    def show_loaded_notification():
+    def show_loaded_notification() -> None:
         """ Show that the sample mod has loaded. """
-        notification = CommonBasicNotification(S4CLSampleModStringId.SAMPLE_MOD_LOADED_NOTIFICATION_TITLE, S4CLSampleModStringId.SAMPLE_MOD_LOADED_NOTIFICATION_DESCRIPTION)
+        notification = CommonBasicNotification(
+            S4CLSampleModStringId.SAMPLE_MOD_LOADED_NOTIFICATION_TITLE,
+            S4CLSampleModStringId.SAMPLE_MOD_LOADED_NOTIFICATION_DESCRIPTION
+        )
         notification.show()
 
     @staticmethod
     @CommonEventRegistry.handle_events('s4cl_sample_mod')
     def _show_loaded_notification_when_loaded(event_data: S4CLZoneLateLoadEvent):
+        if not event_data.game_loaded:
+            # If the game has not loaded yet, we don't want to show our notification.
+            return
         S4CLSampleModShowLoadedMessage.show_loaded_notification()
