73a93b0646e2596863f318031f8dd0fcbf2493de
ColonolNutty <ColonolNutty@hotmail.com>
2021-08-08 10:20:56 -0600

    Updates:
    - Renamed compile_scripts.py to compile.py
    - Some minor clean up of the compile.py script

--- Scripts/compile/compile.py
+++ Scripts/compile/compile.py
@@ -1,8 +1,12 @@
+import os
 from Utilities.unpyc3_compiler import Unpyc3PythonCompiler
+
+
+release_dir = os.path.join('..', '..', 'Release', 'S4CLSampleMod')
 
 # This function invocation will compile the files found within Scripts/s4cl_sample_mod_scripts, put them inside of a file named s4cl_sample_mod.ts4script, and it will finally place that ts4script file within <Project>/Release/S4CLSampleMod.
 Unpyc3PythonCompiler.compile_mod(
+    folder_path_to_output_ts4script_to=release_dir,
     names_of_modules_include=('s4cl_sample_mod_scripts',),
-    folder_path_to_output_ts4script_to='..\\..\\Release\\S4CLSampleMod',
     output_ts4script_name='s4cl_sample_mod'
 )
